09d649411ac028cd32a7ce3bd31296004c0c4f30
MaratYskak <93059311+MaratYskak@users.noreply.github.com>
2024-02-05 07:15:46 +0600

    Update init.py

--- init.py
+++ init.py
@@ -3,5 +3,5 @@ from aiogram import Bot, Dispatcher
 
 
 
-bot = Bot(token="6836716571:AAGKzs1hkP5NlYdR6z3_wQVutV2AcA_N1hY")
+bot = Bot(token="тут вставьте ваш токен (в кавычках)")
 dp = Dispatcher()
